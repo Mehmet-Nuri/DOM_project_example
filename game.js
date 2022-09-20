f32085e4e521a71d0f0bcf3a767064fb5dd16e95
Mehmet-Nuri-0146 <mehmetnuri4601@gmail.com>
2022-09-20 16:15:31 +0300

    v3

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
 //*======================================
 
 //? 1-100 arasinda rasgele bir sayi tut.
-let randomNumber = Math.floor(Math.random() * 100);
+const randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber);
 
 //? Variables
@@ -26,14 +26,26 @@ document.querySelector(".check-btn").addEventListener("click", () => {
   } else if (randomNumber === guessInput) {
     msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i>`;
     body.className = "bg-success";
+    document.querySelector(".check-btn").disabled = true;
     if (score > topScore) {
       topScore = score;
       document.querySelector(".top-score").textContent = topScore;
     }
     document.querySelector(".secret-number").textContent = randomNumber;
   } else {
-    score --;
-    guessInput > randomNumber ? msg.innerText= "DECRASE" : msg.innerText = "INCREASE"
+    score--;
+    if (score > 0) {
+      guessInput > randomNumber
+        ? (msg.innerHTML = `DECRASE <i class="fa-solid fa-arrow-trend-down fa-2x"></i>`)
+        : (msg.innerHTML = `INCREASE <i class="fa-solid fa-arrow-trend-up fa-2x"></i>`);
+    } else {
+      msg.innerHTML = `You lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
+      document.querySelector(".secret-number").textContent = randomNumber;
+      body.className = "bg-danger";
+      document.querySelector(".check-btn").disabled = true;
+    }
+
+    document.querySelector(".score").textContent = score;
   }
 });
 
@@ -44,6 +56,17 @@ document.querySelector(".check-btn").addEventListener("click", () => {
 //! eger rasgele!= input.value
 
 //* again basildiginda oyunu baslangic dgerlerin kur
+document.querySelector(".again-btn").addEventListener("click", () => {
+  score = 10;
+  document.querySelector(".score").textContent = score;
+  const randomNumber = Math.round(Math.random() * 100);
+  document.querySelector(".secret-number").textContent = "?";
+  console.log(randomNumber);
+  document.querySelector(".check-btn").disabled = false;
+ document.querySelector("body").classList.remove("bg-success", "bg-danger");
+ document.querySelector(".guess-input").value = ""
+ document.querySelector(".msg").innerText= `Starting...`
+});
 
 //! LOCALSTORAGE- SESSIONSTORAGE
 
